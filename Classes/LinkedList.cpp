f8475be7982f87c742f01c808766ed1cdfa52368
Polina <irkaxortiza@mail.ru>
2019-02-03 12:55:09 +0300

    Linked List
    was updated

--- Classes/LinkedList.cpp
+++ Classes/LinkedList.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 
-struct Node{
-    Node(int d,Node* n){
+struct Node {
+    Node(int d, Node *n) {
         data = d;
         next = n;
     }
     int data;
-    Node* next;
+    Node *next;
 };
 
-class LinkedList{
+class LinkedList {
 public:
     void add(int num);
-    std::string printLL();
+    std::string toString();
     int remove();
     bool empty();
     void insert(int d, int x);
     int deleteAfter(int x);
     ~LinkedList();
 private:
-      Node* first = nullptr;
+    Node *first = nullptr;
 };
 
+
 void LinkedList::add(int num) {
-    Node* n = new Node(num, first);
+    Node *n = new Node(num, first);
     first = n;
 }
 
-std::string LinkedList::printLL() {
+std::string LinkedList::toString() {
     std::string arrow = " -> ";
     std::string s = " ";
-    Node* node = first;
-    while (node){
+    Node *node = first;
+    while (node) {
         s += std::to_string(node->data);
-        if (node->next){
+        if (node->next) {
             s += arrow;
         }
         node = node->next;
@@ -42,50 +43,60 @@ std::string LinkedList::printLL() {
 }
 
 int LinkedList::remove() {
+    if (empty()){
+        throw std::logic_error("Attempt to delete from empty list");
+    }
     int removed = first->data;
-    Node* n = first;           //
+    Node *n = first;
     first = first->next;
-    delete n;                  //
+    delete n;
     return removed;
 }
 
 bool LinkedList::empty() {
-    if (first){
-        return false;
-    }else{
-        return true;
-    }
+    return first == nullptr;
 }
 
 void LinkedList::insert(int d, int x) {
     auto runner = first;
-    while (runner->data != x){
+    while (runner->data != x) {
         runner = runner->next;
+        if (runner == nullptr) {
+            throw std::logic_error("Using insert function is impossible, number does not exist!");
+        }
     }
     auto last = runner;
-    Node* n = new Node(d,runner->next);
+    Node *n = new Node(d, runner->next);
     last->next = n;
 }
 
 int LinkedList::deleteAfter(int x) {
+    if (empty()){
+        throw std::logic_error("Attempt to delete from empty list");
+    }
     auto runner = first;
-    while (runner->data != x){
+    while (runner->data != x) {
         runner = runner->next;
+        if (runner == nullptr) {
+            throw std::logic_error("Using deleteAfter function is impossible, number does not exist!");
+        }
     }
     auto last = runner;
-    if (runner->next){
+    if (runner->next) {
         auto new_first = runner->next;
         last->next = new_first->next;
-        delete runner->next;           //
-        return new_first->data;
-    }else{
-        return INT_MAX;
+        auto ret = new_first->data;
+        delete new_first;
+        return ret;
+    } else {
+        throw std::invalid_argument("There is no Node!");
     }
+
 }
 
-LinkedList::~LinkedList() {          //
+LinkedList::~LinkedList() {
     auto runner = first;
-    while (runner){
+    while (runner) {
         auto new_first = runner->next;
         delete runner;
         runner = new_first;
@@ -93,7 +104,7 @@ LinkedList::~LinkedList() {          //
 }
 
 
-int main(){
+int main() {
 
     LinkedList l;
 
@@ -104,19 +115,29 @@ int main(){
 
     std::cout << "removed: " << l.remove() << std::endl;
 
-    std::cout << l.printLL() << std::endl;
+    std::cout << l.toString() << std::endl;
 
 
     std::cout << "Is empty? " << l.empty() << std::endl;
 
     l.insert(5, 2);   // 2 ОПЦИЯ (l.insert(5, 3);)
 
-    std::cout << l.printLL() << std::endl;
+    std::cout << l.toString() << std::endl;
 
 
-    std::cout << l.deleteAfter(5) << std::endl;
+//    std::cout << l.deleteAfter(5) << std::endl;
 
-    std::cout << l.printLL() << std::endl;
+    try {
+//        l.remove()    if LL is empty
+//        l.deleteAfter(255)   if LL is empty
+
+        std::cout << l.deleteAfter(5) << std::endl;
+        std::cout << l.deleteAfter(5463783) << std::endl;        // mapping?
+    }catch (const std::exception &exception){
+        std::cout << exception.what() << std::endl;
+    }
+    std::cout << l.toString() << std::endl;
+
 
 
     return 0;
